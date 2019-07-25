afa7f5d80424e36ef566cf1b5f8b0958db9b8c42
artemrakei <artemiuszrakei@gmail.com>
2019-07-25 16:33:09 +0300

    Fourth exercise fix

---
+++ src/main/java/FifthExercise.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FifthExercise {
+    public static void main(String[] args) {
+        int a, sec, min, hours;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number:");
+        a = in.nextInt();
+        sec = a%100;
+        min = (a/100)%100;
+        hours = a/10000;
+        System.out.println(hours + "ч " + min + "мин " + sec + "c");
+    }
+}
