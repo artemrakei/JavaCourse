c3defd45cfdef1b89e7ce7b00f157ed7d93b56ad
artemrakei <artemiuszrakei@gmail.com>
2019-07-25 16:23:56 +0300

    Fourth exercise fix

--- src/main/java/FourthExercise.java
+++ src/main/java/FourthExercise.java
@@ -4,13 +4,15 @@ public class FourthExercise {
     public static void main(String[] args) {
         String str, strRes;
         double a;
-        int b;
+        int b,c;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number :");
         a = in.nextDouble();
         str = Double.toString(a);
+        c=str.length();
         b = str.indexOf('.');
-        strRes = str.substring(0,b-1) + '.' + str.substring(b+1,str.length()-1);
-        System.out.println(strRes);
+        strRes = str.substring(b+1,c) + "." + str.substring(0,b);
+        a = Double.parseDouble(strRes);
+        System.out.println(a);
     }
 }
