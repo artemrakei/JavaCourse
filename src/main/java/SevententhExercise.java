39c6ec4a4aa00188f9d9f7ab3e14389f97a8e349
artemrakei <artemiuszrakei@gmail.com>
2019-07-26 17:01:56 +0300

    Sevententh exercise

---
+++ src/main/java/SevententhExercise.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SevententhExercise {
+    public static void main(String[] args) {
+        char symbol;
+        int number;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter symbol:");
+        symbol = in.next().charAt(0);
+        number = symbol;
+        System.out.println("Number of this number: " + number);
+    }
+}
