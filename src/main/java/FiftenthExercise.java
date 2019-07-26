bbfba0b602144f4eaa60c613ca4d3cf8b84f6c2a
artemrakei <artemiuszrakei@gmail.com>
2019-07-26 15:35:54 +0300

    Fiftenth exercise

---
+++ src/main/java/FiftenthExercise.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class FiftenthExercise {
+    public static int getSquare(int number) {
+
+        int result = number * number;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        BigInteger bigInteger = new BigInteger("1");
+        for (int i = 1; i <= 200; i++) {
+            BigInteger b = new BigInteger(String.valueOf(getSquare(i)));
+            bigInteger = bigInteger.multiply(b);
+        }
+        System.out.println(bigInteger);
+    }
+}
