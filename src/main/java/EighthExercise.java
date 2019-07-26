cbb27f46186380f32a8531dbf1ceee20d7cae2d0
artemrakei <artemiuszrakei@gmail.com>
2019-07-26 10:16:12 +0300

    Eighth exercise

---
+++ src/main/java/EighthExercise.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EighthExercise {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a:");
+        a = in.nextInt();
+        System.out.println("Enter b:");
+        b = in.nextInt();
+        System.out.println("Enter c:");
+        c = in.nextInt();
+        System.out.println("Enter d:");
+        d = in.nextInt();
+        if (a > b) {
+            a = b;
+        }
+        if (c > d) {
+            c = d;
+        }
+        if (a<c) {
+            a=c;
+        }
+        System.out.println(a);
+    }
+}
