4f07f68a0c1ec84195abe3fe064506e8fd6787b5
artemrakei <artemiuszrakei@gmail.com>
2019-07-26 13:50:36 +0300

    Twelfth exercise

---
+++ src/main/java/TwelfthExercise.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TwelfthExercise {
+    public static void main(String[] args) {
+        int a,sum=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number:");
+        a=in.nextInt();
+        for (int i=0;i<=a;i++) {
+            sum+=i;
+        }
+        System.out.println("Your sum = "+sum);
+    }
+}
